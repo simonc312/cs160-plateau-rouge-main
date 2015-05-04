d4614b64da777cd4fab88efbf0465d5579f361b0
jtccheung <jtccheung@berkeley.edu>
2015-05-04 16:12:02 -0700

    Grouped tiles

---
+++ analog-drawing-toy/device/tilefour.js
@@ -0,0 +1,19 @@
+exports.pins = {   
+	x: { type: "A2D" },
+    y: { type: "A2D" },
+    a: { type: "Digital" },
+    s: { type: "Digital" },
+    t: { type: "Digital" },
+};
+
+exports.configure = function() {
+    this.x.init();
+    this.y.init();
+    this.a.init();
+    this.s.init();  
+    this.t.init();  
+}
+
+exports.read = function() {
+    return { x: this.x.read(), y: this.y.read(), a: this.a.read(), s: this.s.read(), t: this.t.read() };
+}
--- analog-drawing-toy/src/main.js
+++ analog-drawing-toy/src/main.js
@@ -119,19 +119,28 @@ Handler.bind("/resetSoldNotifications", Behavior({
 	}
 }));
 
-
-
-Handler.bind("/potResult", Object.create(Behavior.prototype, {
-
+Handler.bind("/tileone", Object.create(Behavior.prototype, {
 	onInvoke: { value: function( handler, message ){
-				application.distribute( "receiveReading", message.requestObject );
-			}}
+		application.distribute( "tileoneReading", message.requestObject );
+	}}
 }));
-Handler.bind("/accelResult", Object.create(Behavior.prototype, {
 
+Handler.bind("/tiletwo", Object.create(Behavior.prototype, {
 	onInvoke: { value: function( handler, message ){
-				application.distribute( "receiveAccelReading", message.requestObject );
-			}}
+		application.distribute( "tiletwoReading", message.requestObject );
+	}}
+}));
+
+Handler.bind("/tilethree", Object.create(Behavior.prototype, {
+	onInvoke: { value: function( handler, message ){
+		application.distribute( "tilethreeReading", message.requestObject );
+	}}
+}));
+
+Handler.bind("/tilefour", Object.create(Behavior.prototype, {
+	onInvoke: { value: function( handler, message ){
+		application.distribute( "tilefourReading", message.requestObject );
+	}}
 }));
 
 Handler.bind("/getTiles", Object.create(Behavior.prototype, {
@@ -200,126 +209,77 @@ MainCanvas.behaviors[0] = function(content, data, dictionary) {
 	Behavior.call(this, content, data, dictionary);
 }
 MainCanvas.behaviors[0].prototype = Object.create(Behavior.prototype, {
-	onDisplaying: { value: 
-
-		function(content) {
-			this.canvas = content;
-            	this.newPoint = true;
-            	this.lastX = false;
-				application.invoke( new MessageWithObject( "pins:/accelerometer/read?repeat=on&callback=/accelResult&interval=70" ) );
-                this.clear();
+	onDisplaying: { value: function(content) {
+			application.invoke( new MessageWithObject( "pins:/tileone/read?repeat=on&callback=/tileone&interval=70" ) );
+			application.invoke( new MessageWithObject( "pins:/tiletwo/read?repeat=on&callback=/tiletwo&interval=70" ) );
+			application.invoke( new MessageWithObject( "pins:/tilethree/read?repeat=on&callback=/tilethree&interval=70" ) );
+			application.invoke( new MessageWithObject( "pins:/tilefour/read?repeat=on&callback=/tilefour&interval=70" ) );
 		},
 	},
-	clear: { value: 
-
-		function(content) {
-			var ctx = this.canvas.getContext( "2d" );
-				var w = this.canvas.width;
-                var h = this.canvas.height;
-      	        ctx.fillStyle = "black";
-				ctx.fillRect( 0,0,w,h );
-				this.newPoint = true;
-		},
-	},
-	receiveReading: { value: 
-
-		function(params, data) {
-			if ( this.newPoint ){
-                	this.startLine( { x: data.xPos, y: data.yPos } );
-                }
-                else {
-                	this.lineTo( { x: data.xPos, y: 1 - data.yPos } );
-                }
-		},
-	},
-	startLine: { value: 
-
-		function(params) {
-			var ctx = this.canvas.getContext( "2d" );
-				var w = this.canvas.width;
-                var h = this.canvas.height;
-                ctx.strokeStyle = "white";
-                ctx.lineWidth = 2;
-                ctx.moveTo( params.x*w, params.y*h ); 
-                ctx.beginPath();
-                this.newPoint = false;
-		},
-	},
-	lineTo: { value: 
-
-		function(params) {
-			var ctx = this.canvas.getContext( "2d" );
-				var w = this.canvas.width;
-                var h = this.canvas.height;
-                ctx.strokeStyle = "white";
-                ctx.lineWidth = 2;
-                ctx.lineTo( params.x*w, params.y*h ); 
-				ctx.stroke();
-		},
-	},
-	receiveAccelReading: { value: 
-
-		function(params, data) {
-			var threshold = .001; 
-				if (activeTiles[0]!=data.a1){
-					addDelivery(0);
-					activeTiles[0] = data.a1;
-				}
-				if (activeTiles[1]!=data.a2){
-					addDelivery(1);
-					activeTiles[1] = data.a2;
-				}
-				if (activeTiles[2]!=data.a3){
-					addDelivery(2);
-					activeTiles[2] = data.a3;
-				}
-				if (activeTiles[3]!=data.a4){
-					addDelivery(3);
-					activeTiles[3] = data.a4;
-				}
-				if (scanTiles[0]!=data.s1){
-					scanTiles[0] = data.s1;
-				}
-				if (scanTiles[1]!=data.s2){
-					scanTiles[1] = data.s2;
-				}
-				if (scanTiles[2]!=data.s3){
-					scanTiles[2] = data.s3;
-				}
-				if (scanTiles[3]!=data.s4){
-					scanTiles[3] = data.s4;
-				}
-				if (stolenTiles[0]!=data.t1){
-					stolenTiles[0] = data.t1;
-				}
-				if (stolenTiles[1]!=data.t2){
-					stolenTiles[1] = data.t2;
-				}
-				if (stolenTiles[2]!=data.t3){
-					stolenTiles[2] = data.t3;
-				}
-				if (stolenTiles[3]!=data.t4){
-					stolenTiles[3] = data.t4;
-				}
-				if ( (tileOneXY[0] != data.x1) || (tileOneXY[1] != data.y1) ){
-					tileOneXY[0] = Math.round(100*data.x1);
-					tileOneXY[1] = Math.round(100*data.y1);
-				}
-				if ( (tileTwoXY[0] != data.x2) || (tileTwoXY[1] != data.y2) ){
-					tileTwoXY[0] = Math.round(100*data.x2);
-					tileTwoXY[1] = Math.round(100*data.y2);
-				}
-				if ( (tileThreeXY[0] != data.x3) || (tileThreeXY[1] != data.y3) ){
-					tileThreeXY[0] = Math.round(100*data.x3);
-					tileThreeXY[1] = Math.round(100*data.y3);
-				}
-				if ( (tileFourXY[0] != data.x4) || (tileFourXY[1] != data.y4) ){
-					tileFourXY[0] = Math.round(100*data.x4);
-					tileFourXY[1] = Math.round(100*data.y4);
-				}
-				
-		},
-	},
+	tileoneReading: { value: function(params, data) {
+	    if (activeTiles[0]!= data.a){
+			addDelivery(0);
+			activeTiles[0] = data.a;
+		}
+		if (scanTiles[0]!=data.s){
+			scanTiles[0] = data.s;
+		}
+		if (stolenTiles[0]!=data.t){
+			stolenTiles[0] = data.t;
+		}
+		if ( (tileOneXY[0] != data.x) || (tileOneXY[1] != data.y) ){
+			tileOneXY[0] = Math.round(100*data.x);
+			tileOneXY[1] = Math.round(100*data.y);
+		}
+	}},
+	tiletwoReading: { value: function(params, data) {
+	    if (activeTiles[1]!= data.a){
+			addDelivery(1);
+			activeTiles[1] = data.a;
+		}
+		if (scanTiles[1]!=data.s){
+			scanTiles[1] = data.s;
+		}
+		if (stolenTiles[1]!=data.t){
+			stolenTiles[1] = data.t;
+		}
+		if ( (tileTwoXY[0] != data.x) || (tileTwoXY[1] != data.y) ){
+			tileTwoXY[0] = Math.round(100*data.x);
+			tileTwoXY[1] = Math.round(100*data.y);
+		}
+	}},
+	tilethreeReading: { value: function(params, data) {
+	    if (activeTiles[2]!= data.a){
+			addDelivery(2);
+			activeTiles[2] = data.a;
+		}
+		if (scanTiles[2]!=data.s){
+			scanTiles[2] = data.s;
+		}
+		if (stolenTiles[2]!=data.t){
+			stolenTiles[2] = data.t;
+		}
+		if ( (tileThreeXY[0] != data.x) || (tileThreeXY[1] != data.y) ){
+			tileThreeXY[0] = Math.round(100*data.x);
+			tileThreeXY[1] = Math.round(100*data.y);
+		}
+	}},
+	tilefourReading: { value: function(params, data) {
+	    if (activeTiles[3]!= data.a){
+			addDelivery(3);
+			activeTiles[3] = data.a;
+		}
+		if (scanTiles[3]!=data.s){
+			scanTiles[3] = data.s;
+		}
+		if (stolenTiles[3]!=data.t){
+			stolenTiles[3] = data.t;
+		}
+		if ( (tileFourXY[0] != data.x) || (tileFourXY[1] != data.y) ){
+			tileFourXY[0] = Math.round(100*data.x);
+			tileFourXY[1] = Math.round(100*data.y);
+		}
+	}},
 });
 
 //ADD TO MAIN COMPONENTS
@@ -327,32 +287,46 @@ var mainCanvas = new MainCanvas();
 application.add( mainCanvas );
 
 application.invoke( new MessageWithObject( "pins:configure", {
-
-    accelerometer: {
-        require: "accelerometer",
+    tilefour: {
+        require: "tilefour",
         pins: {
-            x1: { pin: 15 }, 
-			y1: { pin: 16 }, 
-			a1: { pin: 17 }, 
-			s1: { pin: 18 }, 
-			
-			x2: { pin: 19 }, 
-			y2: { pin: 20 }, 
-			a2: { pin: 21 }, 
-			s2: { pin: 22 }, 
-			
-			x3: { pin: 23 }, 
-			y3: { pin: 24 }, 
-			a3: { pin: 3 }, 
-			s3: { pin: 4 }, 
-			
-			x4: { pin: 5 }, 
-			y4: { pin: 6 }, 
-			a4: { pin: 7 }, 
-			s4: { pin: 8 }, 
-			
+            x: { pin: 25 },
+            y: { pin: 26 },
+            a: { pin: 27 },
+            s: { pin: 28 },
+            t: { pin: 29 },
         }
-    }
+    },
+    tilethree: {
+        require: "tilethree",
+        pins: {
+            x: { pin: 35 },
+            y: { pin: 36 },
+            a: { pin: 37 },
+            s: { pin: 38 },
+            t: { pin: 39 },
+        }
+    },
+    tiletwo: {
+        require: "tiletwo",
+        pins: {
+            x: { pin: 9 },
+            y: { pin: 10 },
+            a: { pin: 11 },
+            s: { pin: 12 },
+            t: { pin: 13 },
+        }
+    },
+    tileone: {
+        require: "tileone",
+        pins: {
+            x: { pin: 30 },
+            y: { pin: 31 },
+            a: { pin: 32 },
+            s: { pin: 33 },
+            t: { pin: 34 },
+        }
+    },
 }));
 
 //APPLICATION BEHAVIOR 
